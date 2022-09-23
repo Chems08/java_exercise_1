11378781e74ea3cd33459401c9b4d007a71f368e
Chems08 <cmitta1234@gmail.com>
2022-09-23 19:27:38 +0200

    commit 7

--- src/Launcher.java
+++ src/Launcher.java
@@ -6,11 +6,20 @@ public class Launcher {
     
 
     public static int fibo(int n){
-        if (n == 0) {return 0;}
-        else if (n == 1) {return 1;}
-        else {return fibo(n-1)+fibo(n-2);}
-    }   
-
+        if(n <= 1) {
+			return n;
+		}
+		int fib = 1;
+		int fib2 = 1;
+		
+		for(int i=2; i<n; i++) {
+			int temp = fib;
+			fib+= fib2;
+			fib2 = temp;
+		}
+		return fib;
+    }
+        
 
     public static List freq() {
         List<String> myList5 = new ArrayList<String>();
